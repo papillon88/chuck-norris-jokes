dd759527fb23f9a699c811a43f173b6da77c6662
papillon <devopreet.ch@gmail.com>
2017-08-04 10:14:38 +0530

    Initial commit

---
+++ src/main/java/qwe/asd/chucknorrisjokes/ChuckNorrisJokesApplication.java
@@ -0,0 +1,12 @@
+package qwe.asd.chucknorrisjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisJokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckNorrisJokesApplication.class, args);
+	}
+}
