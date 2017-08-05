148e03f9590ca7771f3b544b69d24c03450eb767
papillon <devopreet.ch@gmail.com>
2017-08-05 15:55:52 +0530

    added external properties file

--- src/main/java/qwe/asd/chucknorrisjokes/ChuckNorrisJokesApplication.java
+++ src/main/java/qwe/asd/chucknorrisjokes/ChuckNorrisJokesApplication.java
@@ -2,12 +2,18 @@ package qwe.asd.chucknorrisjokes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ImportResource;
+import qwe.asd.chucknorrisjokes.repositories.FakeDataSource;
 
 @SpringBootApplication
 public class ChuckNorrisJokesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChuckNorrisJokesApplication.class, args);
+
+		ApplicationContext ctx = SpringApplication.run(ChuckNorrisJokesApplication.class, args);
+		FakeDataSource fakeDataSource =
+				(FakeDataSource)ctx.getBean(FakeDataSource.class);
+		System.out.println(fakeDataSource.getUser());
 	}
 }
